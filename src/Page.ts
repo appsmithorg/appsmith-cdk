3d1243d048d6dcfd536c886619c1332e737d9807
Arpit Mohan <mohanarpit@users.noreply.github.com>
2023-07-13 19:42:34 +0530

    feat: Add application and page object (#1)

--- src/Page.ts
+++ src/Page.ts
@@ -1,24 +1,52 @@
-import {Appsmith} from "~/Appsmith";
-import {JsObject} from "~/JsObject";
+import { JsObject } from "~/JsObject";
 
-class Page extends Appsmith {
-    private widgets: Array<Object>;
-    private customJSFunctions: Array<JsObject>;
-    
-    constructor(name: string) {
-        super(name);
-        this.widgets = new Array<Object>;
-        this.customJSFunctions = new Array<JsObject>;
-    }
+class Page {
+  // private widgets: Array<Object>;
+  // private customJSFunctions: Array<JsObject>;
 
-    addWidget(widget: Object): void{
-        this.widgets.push(widget);
-    }
+  private name: string;
+  private slug: string;
+  private layouts = [
+    {
+      viewMode: false,
+      dsl: null,
+      layoutOnLoadActions: null,
+      layoutOnLoadActionErrors: null,
+      validOnPageLoadActions: null,
+      deleted: false,
+      policies: null,
+      userPermissions: null,
+      id: null,
+    },
+  ];
 
-    addCustomJSFunc(customJSFunc: JsObject): void{
-        this.customJSFunctions.push(customJSFunc);
-    }
+  private userPermissions = null;
+  private policies = null;
+  private isHidden = false;
 
+  constructor(name: string) {
+    // this.widgets = new Array<Object>;
+    // this.customJSFunctions = new Array<JsObject>;
+    this.name = name;
+    this.slug = name;
+  }
+
+  setSlug(slug: string) {
+    this.slug = slug;
+    return this;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  // addWidget(widget: Object): void{
+  //     this.widgets.push(widget);
+  // }
+
+  // addCustomJSFunc(customJSFunc: JsObject): void{
+  //     this.customJSFunctions.push(customJSFunc);
+  // }
 }
 
 export default Page;
