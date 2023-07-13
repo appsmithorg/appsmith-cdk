1f7054c5b96dd9427fbf9f384ba18d4a5b3d40ad
Hetu Nandu <hetunandu@gmail.com>
2023-07-13 18:55:27 +0530

    Base widget and Button widget

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
-import { JsObject } from "~/JsObject";
-import Widget from "~/Widget";
-import {Button} from "~/widgets/Button";
+import { Button } from "~/widgets/Button";
 
+const button = new Button("SubmitButton");
 
-const js = new JsObject("Js");
+button.setText("Submit")
+.setVariant("SECONDARY");
 
-js.setSettingFor("run").onLoad(true).confirm(true)
-
-console.log(js);
-
-const widget = new Button();
-
-widget._onClick = "Run Api 1";
-widget.type = "ButtonWidget"
-widget.visible = true;
-
-console.log(JSON.stringify(widget))
+console.log({ button });
