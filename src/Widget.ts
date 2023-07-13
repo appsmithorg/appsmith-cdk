62fcb862eb289434bc53a33cc4b8ee7dee6c4cc3
Hetu Nandu <hetunandu@gmail.com>
2023-07-13 18:55:27 +0530

    Base widget and Button widget

--- src/Widget.ts
+++ src/Widget.ts
@@ -1,12 +1,69 @@
 import {Appsmith} from "~/Appsmith";
+import { generateKey } from "~/utils";
+
+export enum WidgetType {
+    TEXT = "TEXT_WIDGET",
+    CONTAINER = "CONTAINER_WIDGET",
+    CANVAS = "CANVAS_WIDGET",
+    INPUT = "INPUT_WIDGET_V2",
+    BUTTON = "BUTTON_WIDGET",
+
+}
+
 
 class Widget extends Appsmith {
-    type: string;
-    visible: boolean;
-    // TODO add basic properties
+    /** Operational properties  */
+    type: WidgetType;
+    dynamicTriggerPathList: Array<{ key: string }>;
+    dynamicBindingPathList: Array<{ key: string }>;
+    key = generateKey({ size: 5 });
+    widgetId = generateKey({ size: 10 });
+    isDeprecated = false;
+    renderMode = "CANVAS";
+    animateLoading = true;
+    isLoading = false;
+    /** Functional properties  */
+    widgetName: string;
+    isVisible: true;
+    version: number;
+    /** Position properties  */
+    // Responsive position
+    mobileBottomRow: number;
+    mobileRightColumn: number;
+    mobileTopRow: number;
+    mobileLeftColumn = 0.0;
+    maxDynamicHeight = 9000.0;
+    minDynamicHeight = 4.0;
+    dynamicHeight = "AUTO_HEIGHT";
+    responsiveBehavior: "fill" | "hug";
+    // Fixed position
+    topRow = 0.0;
+    bottomRow = 0.0;
+    parentRowSpace = 10.0;
+    parentColumnSpace = 16.5625;
+    leftColumn = 0.0;
+    rightColumn = 64.0;
+    minWidth: number;
 
-    constructor(name?: string) {
+    /** Temporary properties */
+    parentId: string;
+    height: number;
+
+
+    constructor(name: string, type: WidgetType) {
         super(name);
+        this.widgetName = name;
+        this.type = type;
+    }
+
+    setHeight (number) {
+        this.height = number
+        return this;
+    }
+
+    setResponsiveBehaviour(behaviour: "fill" | "hug") {
+        this.responsiveBehavior = behaviour;
+        return this;
     }
 
 }
--- src/widgets/Button.ts
+++ src/widgets/Button.ts
@@ -1,11 +1,63 @@
-import Widget from "~/Widget";
+import Widget, {WidgetType} from "~/Widget";
 
 export class Button extends Widget {
-  set _onClick(value: string) {
-    this.onClick = value;
+  resetFormOnClick: boolean;
+  disabledWhenInvalid: boolean;
+  boxShadow = "none";
+  buttonColor = "{{appsmith.theme.colors.primaryColor}}";
+  borderRadius = "{{appsmith.theme.borderRadius.appBorderRadius}}";
+  text: string;
+  isDefaultClickDisabled = true;
+  recaptchaType = "V3";
+  version = 1.0;
+  dynamicBindingPathList = [
+    {
+      "key": "buttonColor"
+    },
+    {
+      "key": "borderRadius"
+    }
+  ];
+
+  buttonVariant: "PRIMARY" | "SECONDARY" | "TERTIARY" = "PRIMARY";
+  placement: "START" | "BETWEEN" | "CENTER" = "CENTER";
+  alignment = "center";
+
+  constructor(name: string) {
+    super(name, WidgetType.BUTTON)
+    this.setHeight(17);
+    this.setResponsiveBehaviour("hug");
   }
-  private onClick: string;
-  constructor(name?: string) {
-    super(name)
+
+  isDisabled: string | boolean;
+
+  setText(text: string) {
+    this.text = text;
+    return this;
+  }
+
+  setIsDisabled(isDisabled: string | boolean) {
+    this.isDisabled = isDisabled;
+    return this;
+  }
+
+  setVariant(variant: "PRIMARY" | "SECONDARY" | "TERTIARY") {
+    this.buttonVariant = variant;
+    return this;
+  }
+
+  setPlacement(placement: "START" | "BETWEEN" | "CENTER") {
+    this.placement = placement;
+    return this;
+  }
+
+  setResetFormOnClick(resetForm: boolean) {
+    this.resetFormOnClick = resetForm;
+    return this;
+  }
+
+  setDisabledWhenInvalid(disabled: boolean) {
+    this.disabledWhenInvalid = disabled;
+    return this;
   }
 }
